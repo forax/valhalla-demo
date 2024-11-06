0c01c57f15c00e44a80c4c613c430c12437fd08f
forax <forax@univ-mlv.fr>
2024-11-06 10:23:52 +0100

    simplify the example

--- src/main/java/_6_nullness.java
+++ src/main/java/_6_nullness.java
@@ -4,9 +4,6 @@ void main() {
   //var village = new Village(population);
   //var kvillage = new KVillage(population);
 
-  //var village2 = new Village(null);
-  //var kvillage2 = new KVillage(null);
-
   //var capital = new Capital(population);
   //var kcapital = new KCapital(population);
 }
